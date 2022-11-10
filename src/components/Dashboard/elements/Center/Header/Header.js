49f2daccda93392ad0dc7501475a632a48425cd4
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-10 12:28:04 -0400

    ⏳ ARREGLO Haciendo otro header para recibir botones y el titulo de la página

--- src/components/Dashboard/elements/Center/Header/Header.js
+++ src/components/Dashboard/elements/Center/Header/Header.js
@@ -1,10 +1,17 @@
 import './Header.scss'
 
-export default function Header({h1Title}){
+export default function Header({h1Title, description}){
     return(
-        <header>
-            <h1>{h1Title}</h1>
-            
-        </header>
+        <>
+            <header>
+                <button id='btnReturnHome'/>
+                <h1>{h1Title}</h1>
+                <div>
+                    <button id='btnTheme'/>    
+                    <button id='btnNotifications'/>    
+                </div>
+            </header>
+        </>
+        
     )
 }
