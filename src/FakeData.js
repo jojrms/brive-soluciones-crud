21871cbacdc92ae9866344977ac847c9c261e240
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-10 09:35:38 -0400

    💣 CODIFICACIÓN Creando fake users para hacer prueba de .map

---
+++ src/FakeData.js
@@ -0,0 +1,23 @@
+export const Users = [
+    {
+        id: 1,
+        name: 'Jordana Ramos',
+        userName: 'jojrms',
+        email: 'jordanarmos@gmail.com',
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    },
+    {
+        id: 2,
+        name: 'Jordana Ramos',
+        userName: 'jojrms',
+        email: 'jordanarmos@gmail.com',
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    },
+    {
+        id: 3,
+        name: 'Jordana Ramos',
+        userName: 'jojrms',
+        email: 'jordanarmos@gmail.com',
+        urlPhoto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+    }
+]
