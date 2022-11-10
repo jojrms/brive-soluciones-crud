553bc8a57be482bae328a9655d02953400bfaba9
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-10 11:59:25 -0400

    Enviando los datos de los usuarios al LocalStorage en el archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Users } from './FakeData';
+
 import Dashboard from './components/Dashboard/Dashboard';
 
 import './components/General.css'
 
 function App() {
+
+  // Poniendo el array con los usuarios fakes en el localStorage 
+  // con la llave 'fakeUsers'
+  localStorage.setItem('fakeUsers', JSON.stringify(Users));
+
   return (
     <div className="App">
       <Dashboard/>
