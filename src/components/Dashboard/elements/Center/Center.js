6bf63a57c472502f4c0bbd50dd698fce98555b01
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-10 10:13:59 -0400

    💣 CODIFICACIÓN LLamando la lista con fake user y haciendo un .map en "sectionListUser", para exhibir los usuarios

--- src/components/Dashboard/elements/Center/Center.js
+++ src/components/Dashboard/elements/Center/Center.js
@@ -1,17 +1,32 @@
-import Header from './Header/Header'
+import {Users} from '../../../../FakeData'
+
+import Header from './Header/Header';
+import UsersViewer from './Users/UsersViewer';
 
 import './Center.scss'
 
 export default function Center(){
 
+    console.log(Users);
+
     return(
         <aside>
             <div className='divImageDecorationAbsolute'/>
             <Header
-                h1Title={"Empleados"} 
-                h2Title={"Total de Empleados"}
-                qtd={45}
+                h1Title={"Empleados"}
             />
+            <section className='sectionListUsers'>
+                {Users.map((user) => {
+                    return(
+                    <UsersViewer
+                        key={user.id}
+                        name={user.name}
+                        urlPhoto={user.urlPhoto}
+                    />
+                    )
+                })}
+                
+            </section>
         </aside>
     )
 }
