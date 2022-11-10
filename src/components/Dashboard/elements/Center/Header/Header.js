d9b821635fb600292eb911b776e56aa6bee139d8
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-10 13:21:06 -0400

    💣 CODIFICACIÓN Cambiando la estructura del header 1. Poniendo el perfil del usuario en el header 2. Agregando button para search de empleado

--- src/components/Dashboard/elements/Center/Header/Header.js
+++ src/components/Dashboard/elements/Center/Header/Header.js
@@ -1,15 +1,24 @@
 import './Header.scss'
 
-export default function Header({h1Title, description}){
+export default function Header({h1Title, userLogPhotoUrl, userLogName}){
     return(
         <>
             <header>
                 <button id='btnReturnHome'/>
+
                 <h1>{h1Title}</h1>
+
                 <div>
+                    <button id='btnSearch'/>
                     <button id='btnTheme'/>    
                     <button id='btnNotifications'/>    
                 </div>
+
+                <div className='divProfileExb'>
+                    <span style={{backgroundImage: `url(${userLogPhotoUrl})`}} className='spanPhotoProfile'/> 
+                    <p>{userLogName}</p>
+                </div>
+
             </header>
         </>
         
