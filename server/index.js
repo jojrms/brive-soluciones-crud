e7acf84dc28584a2326aef87b3b703ee008cd42b
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-11 20:40:49 -0400

    💣 CODIFICACIÓN Cambiando la base de datos del server de mysql para mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,41 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-})
+const UserModel = require('./models/User');
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    'mongodb+srv://admin:password123a@crud.bfowslq.mongodb.net/users?retryWrites=true&w=majority', 
+    {
+        useNewUrlParser: true,
+    }
+);
+
+app.post("/createUser", async(req, res) => {
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const url_photo = req.body.url_photo;
+
+    const user = new UserModel({
+        name: name,
+        email: email,
+        phone: phone,
+        url_photo: url_photo,
+    });
+
+    try{
+        await user.save()
+    } catch(err){
+        console.log(err);
+    }
+});
 
 app.listen(3001, () => {
-    console.log('funcionando');
+    console.log('server funcionando');
 })
